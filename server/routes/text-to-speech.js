c4471d7e2133eb0637a9b74747af9c7ce626f4fe
parsamzzz <parsa.pano@gmail.com>
2025-09-15 02:24:36 +0330

    429-2

--- server/routes/text-to-speech.js
+++ server/routes/text-to-speech.js
@@ -7,7 +7,6 @@ const router = express.Router();
 const API_KEYS = [
 
 
-'AIzaSyA73WazXgApiGxxNIUkNLS6HH58FwnytlY',
 'AIzaSyBn1Fm_OtU0cWxo4MkpnrDyBJn9HXlwABQ',
 'AIzaSyDGBqiqsW9U1mivsAwh5vMU0ZARP8E-uvM',
 'AIzaSyCa_2ERS8aAM5pKFa1xOhwsyJ_IpXxmEdU',
