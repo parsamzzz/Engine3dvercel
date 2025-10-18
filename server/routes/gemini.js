0ddba0ed1be79538c260d91f4800c8f7540fa14c
parsamzzz <parsa.pano@gmail.com>
2025-10-18 22:38:34 +0330

    sdfdsf

--- server/routes/gemini.js
+++ server/routes/gemini.js
@@ -10,6 +10,7 @@ const upload = multer();
 // 🔑 همه کلیدها
 // =====================
 const API_KEYS = [
+  "AIzaSyDgX7F-P_9hjuLMDOIhNQffyAf_0Tp4gPU",
   "AIzaSyAM6ZzH-ngfxY1wKTnoHVMyD2kYOHLd1i4",
    "AIzaSyCMmOaJFfHY2PnvNe2jAJ8gLb8ToFQxUMc",
     "AIzaSyC2Z4aD2ZSlpHem3BzA5u7GX8nM-Py5abw",
